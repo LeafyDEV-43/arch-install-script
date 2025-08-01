7595e0618eccdcc2117340c21d29b62d133f012f
LeafyDev <102246549+LeafyDEV-43@users.noreply.github.com>
2025-08-01 09:41:12 +0200

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,57 +1,30 @@
 #!/bin/bash
-# post-install.sh -- Runs inside chroot
 set -e
 
-USERNAME=$1
-PASSWORD=$2
+USERNAME="$1"
+PASSWORD="$2"
 
-# Timezone & locale
-echo ">>> Setting timezone..."
+echo ">>> Updating system and installing GUI packages..."
+pacman -Syu --noconfirm
+pacman -S --noconfirm plasma kde-applications ly kitty dolphin firefox \
+    neovim btop mpv code neofetch
+
+echo ">>> Creating user and setting password..."
+useradd -m -G wheel "$USERNAME"
+echo "$USERNAME:$PASSWORD" | chpasswd
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo ">>> Enabling essential services..."
+systemctl enable NetworkManager
+systemctl enable ly
+systemctl set-default graphical.target
+
+echo ">>> Setting hostname and locale..."
+echo "$USERNAME-PC" > /etc/hostname
 ln -sf /usr/share/zoneinfo/Africa/Lusaka /etc/localtime
 hwclock --systohc
-
-echo ">>> Configuring locale..."
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# Hostname and hosts
-echo "archvm" > /etc/hostname
-echo -e "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\tarchvm.localdomain archvm" > /etc/hosts
-
-# Root password
-echo "root:$PASSWORD" | chpasswd
-
-# Create user
-echo ">>> Creating user $USERNAME..."
-useradd -m -G wheel -s /bin/bash $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
-
-# Install and enable services
-echo ">>> Enabling services..."
-systemctl enable NetworkManager
-systemctl enable ly
-
-# Bootloader
-echo ">>> Installing GRUB..."
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Autostart neofetch
-mkdir -p /home/$USERNAME/.config/autostart
-cat <<EOF > /home/$USERNAME/.config/autostart/neofetch.desktop
-[Desktop Entry]
-Type=Application
-Exec=kitty --hold -e neofetch
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true
-Name=Neofetch
-Comment=Run neofetch on login
-EOF
-
-chown -R $USERNAME:$USERNAME /home/$USERNAME
-
-echo ">>> Post-install complete! Ready to reboot."
-exit
+echo ">>> Done! Exit and reboot when ready."
